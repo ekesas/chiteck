495c5f38091900c4ff1c28f9e4e0b00b9ddc62a2
ekesas <ekesas@gmail.com>
2015-01-15 10:50:00 -0600

    modified the home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,89 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+
+
+var message = "We create interactive, user-friendly, high-performance, well-integrated web 
+applications and solutions, both “standalone” or as part of a multi-level system, that help 
+companies solve everyday business tasks and overcome challenges of the digital era. 
+Softeq carefully takes you through every step of the process — from specification and 
+prototyping stages to deployment and ongoing support."
+
+
+
+function change(){
+ document.getElementById("java");
+ java.innerHTML = message;
+}
+
+function change2(){
+ document.getElementById("java");
+ java.innerHTML = "Who We Are";
+}
+
+function simu(){
+	document.getElementById("phone");
+	phone.innerHTML ="I love this";
+}
+
+function simu2(){
+	document.getElementById("phone");
+	phone.InnerHTML = "We also do this";
+}
+
+function highlight(){
+	document.getElementById("color")
+	color.innerHTML = "Give us a call. Let's see what we can do for you";
+}
+
+function highlight2(){
+	document.getElementById("color")
+	color.innerHTML = "Your Small Project is Our Speciality";
+}
+
+function nyumbani(){
+	document.getElementById("home")
+	home.innerHTML = "Nyumbani";
+}
+
+function nyumbani2(){
+	document.getElementById("home")
+	home.innerHTML = "Home";
+}
+
+function huduma(){
+	document.getElementById("services")
+	services.innerHTML = "Huduma";
+}
+
+function huduma2(){
+	document.getElementById("services")
+	services.innerHTML = "Services";
+}
+
+function wateja(){
+	document.getElementById("clients")
+	clients.innerHTML = "Wateja";
+}
+
+function wateja2(){
+	document.getElementById("clients")
+	clients.innerHTML = "Clients";
+}
+
+function mawasiliano(){
+	document.getElementById("contacts")
+	contacts.innerHTML = "Mawasiliano";
+}
+
+function mawasiliano2(){
+	document.getElementById("contacts")
+	contacts.innerHTML = "Contacts";
+
+}
+
+
+
